7bb85c282bb90b842c2e789121c0e1816ae57e9e
James Bonfield <jkb@sanger.ac.uk>
2016-08-02 22:55:48 +0100

    Add extern "C" for C++.

--- rANS_static.h
+++ rANS_static.h
@@ -1,6 +1,10 @@
 #ifndef RANS_STATIC_H
 #define RANS_STATIC_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 unsigned int rans_compress_bound(int renorm_size,
 				 unsigned int size, int order);
 unsigned char *rans_compress_to(int renorm_size,
@@ -17,5 +21,9 @@ unsigned char *rans_uncompress_to(int renorm_size,
 unsigned char *rans_uncompress(int renorm_size,
 			       unsigned char *in, unsigned int in_size,
 			       unsigned int *out_size, int order);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* RANS_STATIC_H */
